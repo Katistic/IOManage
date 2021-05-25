8cc8327bd2fb0705b45671187feffded2be48b37
Katistix <33645731+Katistic@users.noreply.github.com>
2021-05-25 12:28:25 +1000

    Version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="iomanage",
-    version="1.0.5",
+    version="1.1.0",
     description="Manages IO operations of a single file",
     long_description=README,
     long_description_content_type="text/markdown",
